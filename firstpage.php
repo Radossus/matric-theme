83d9e8bc7de2650aef68a65394e4f545f73d4b3b
Radek Slachta <spool@seznam.cz>
2021-01-12 21:50:13 +0100

    zmena

---
+++ firstpage.php
@@ -0,0 +1,43 @@
+﻿<?php
+/**
+* Template Name: Úvodní strana TESTTTTT
+* Description: Homepage
+*/
+?>
+<?php get_header(); ?>
+<div id="landing-intro">
+    <div class="container" id="intro-text">
+        <div id = "card-info" class="card border-0 w-50" >
+            <div class="card-header" id="landing-header">
+                <div>Solution Engineer</div>
+            </div>
+            <div class="card-body" id="landing-text">
+                <p>Poskytuji služby v oblasti podnikových informačních systémů. Mým cílem je navrhnout a udržovat spolehlivou infrastrukturu informačních a komunikačních technologií, která pokrývá celou řadu požadavků stanovených IT strategií.</p>
+                <div>
+                    <button type="button" class="btn btn-success btn-lg">Nabízené služby</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <?php while(have_posts()) : the_post(); ?>
+
+        <?php the_content(); ?>
+
+    <?php endwhile; wp_reset_query();?>
+</div>
+
+
+
+<script>
+    $( function() {
+        $( "#landing-intro" ).hide();
+        $( "#landing-intro" ).fadeIn(900);
+        $( "#landing-header" ).hide();
+        $( "#landing-header" ).fadeIn(600);
+        $( "#landing-text" ).hide();
+        $( "#landing-text" ).fadeIn(1000);
+    } );
+</script>
---
+++ header.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href= "<?php bloginfo('stylesheet_url'); ?>" rel ="stylesheet" type="text/css" media="screen,projection">
+    <!--<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" /> -->
+    <meta name="description" content="Oficiální stránky.">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?php bloginfo('name'); ?></title>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
+  </head>
+<body>
+<header></header>
+                
