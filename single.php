a5e8c1fd2768dbf13d7a9d0a70b0694d6d186166
Radek-Slachta <radek.slachta@heimstaden.com>
2022-12-29 21:19:38 +0100

    adding variable definition

--- single.php
+++ single.php
@@ -24,6 +24,7 @@
         <div class="text-center">
             <a href="<? echo get_home_url().'/blog'; ?>" class="btn btn-success btn-lg m-1" role ="button">Všechny články</a>
             <?php
+            $output = '';
             $categories = get_categories();
 
             if ( $categories ) {
